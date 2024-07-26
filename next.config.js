2aabd40ce05653c89fa447c6fe7b762197da4a72
dieverkochka <165576120+dieverkochka@users.noreply.github.com>
2024-07-27 00:22:11 +0300

    Update and rename next.config.js to next.config.mjs

--- next.config.js
+++
@@ -1,8 +0,0 @@
-import { withContentlayer } from "next-contentlayer";
-
-const nextConfig = {
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  experimental: {
-    mdxRs: false,
-  },
-};
