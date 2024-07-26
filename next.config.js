b399be584c88af0e308acce43cdb6ffd88de8704
dieverkochka <165576120+dieverkochka@users.noreply.github.com>
2024-07-27 00:16:46 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ import { withContentlayer } from "next-contentlayer";
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   experimental: {
-    mdxRs: true,
+    mdxRs: false,
   },
 };
 
